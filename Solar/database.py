c2972d714a8663ca3056ef9a6caf53cc899b09ff
Harsh Sharma <133791781+HarshSharmaPython@users.noreply.github.com>
2024-11-01 22:50:25 +0530

    dd

--- Solar/database.py
+++ Solar/database.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 # Fetch the MongoDB URI from environment variables
-uri = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')  # Default to local if not set
+uri = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')  # Fallback for local development
 client = MongoClient(uri)
 
 # Specify your database
